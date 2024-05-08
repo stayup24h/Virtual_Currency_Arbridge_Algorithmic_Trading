fb96c531be7c129f0fcc74be001d3bfa79012a6a
stayup24h <sani0501we@gmail.com>
2024-05-09 01:07:15 +0900

    ♻️ refactor: change called api on BinanceCoin's getData

--- binanceApi.py
+++ binanceApi.py
@@ -1,12 +1,12 @@
 import requests
 import pandas as pd
 
-class Binance: #binance api class, get 계열(코인 가격, 거래량등 개인정보 필요x)
+class BinanceCoin: #binance api class, get 계열(코인 가격, 거래량등 개인정보 필요x)
         def getData(self, target_currency): #코인 평균가격 가져오는 함수
-            self.url = "https://api.binance.com/api/v3/avgPrice"
+            self.url = "https://api.binance.com/api/v3/ticker/price"
             
             self.params = {
-                  "symbol": target_currency
+                  "symbol": target_currency+"USDT"
             }
 
             res = requests.get(self.url, params=self.params)
